0fcf31d4ab4fb941592f8b6ceeac1b360480b51f
MARCO <jmukhammadsodikh@gmail.com>
2026-05-01 10:22:36 +0900

    feat: thinking about fullstack

---
+++ notice.js
@@ -0,0 +1,14 @@
+/*
+Fullstack = Restaurant
+
+Frontend Development 2 ways:
+  - BSSR(backend server side rendoring) = (DTE/JTE/EJS) = making frontend in backend 
+  - SPA (React/Angular/Vue) = single page application
+
+API REQUEST:
+   - Design    > Traditional API | Rest APi | GraphQL API
+   - Method    > GET(get info) | POST(mutate info)
+   - Structure > header | body
+  
+  */
+
---
+++ server.js
@@ -0,0 +1,2 @@
+// Backend server  NodeJs = via EXPRESS NESTJS framework
+// Backend server Python = via DJANGO FastAPI
